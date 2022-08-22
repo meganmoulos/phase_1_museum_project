36ed27c3045c26ec6d7f3b74585c53020f9980e1
Megan <meganmoulos@gmail.com>
2022-08-22 16:22:08 -0700

    changed fave button, need to fix

--- src/script.js
+++ src/script.js
@@ -1,30 +1,34 @@
 // Globals
-const url = 'https://api.artic.edu/api/v1/artworks/?page=3&limit=4'
+const url = 'https://api.artic.edu/api/v1/artworks/?page=2&limit=3'
 let selectedArtwork;
 
 // DOM Selectors
 const largeImg = document.querySelector('#largeImage')
 const unorderedList = document.querySelector('#rightMenuList')
+const leftContainer = document.querySelector('#leftContainer')
 
 // Event Listeners
 
 
 // Render Functions
 function renderAnArtwork(artwork){
-    console.log(artwork)
     selectedArtwork = artwork
     largeImg.src = getImageSource(artwork.image_id)
     artistName.textContent = artwork.artist_title
     artworkTitle.textContent = artwork.title
     artworkDescription.textContent = artwork.credit_line
+    let faveButton = document.createElement('button')
+    faveButton.textContent = 'Favorite'
+    leftContainer.appendChild(faveButton)
+    faveButton.addEventListener('click', changeFavorite)
 }
 
 function renderRightImage(artwork){
-    console.log(artwork)
     selectedArtwork = artwork
     const newLi = document.createElement('li')
     const newImg = document.createElement('img')
     newImg.src = getImageSource(artwork.image_id)
+    newImg.setAttribute('id', 'rightImg')
     newLi.appendChild(newImg)
     newLi.onclick = () => {
         renderAnArtwork(artwork);
@@ -42,6 +46,11 @@ function getImageSource(imgId){
 
 function changeImage() {
     renderAnArtwork(selectedArtwork)
+}
+
+function changeFavorite(e){
+    e.target.style.background = 'red'
+    e.target.style.color = 'white'
 }
 
 // function to add selected artwork to cart
