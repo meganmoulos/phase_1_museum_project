0af854d791c0b8992e5c757df2e513dc7785f5a7
Megan <meganmoulos@gmail.com>
2022-08-22 14:05:17 -0700

    attemping to edit

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 // Globals
-const url = 'https://api.artic.edu/api/v1/artworks/?page=3&limit=50'
+const url = 'https://api.artic.edu/api/v1/artworks/?page=3&limit=12'
 
 // DOM Selectors
 const largeImg = document.querySelector('#largeImage')
@@ -13,7 +13,6 @@ firstRightImage.addEventListener('click', renderAnArtwork);
 // Render Functions
 function renderAnArtwork(artwork){
     console.log(artwork)
-    console.log(artwork.image_id)
     let imgId = artwork.image_id
     largeImg.src = `https://www.artic.edu/iiif/2/${imgId}/full/400,/0/default.jpg`
     artistName.textContent = artwork.artist_title
@@ -37,9 +36,6 @@ function rightMenu(artwork, artwork2, artwork3){
     thirdRightImage.src = `https://www.artic.edu/iiif/2/${imageId3}/full/200,/0/default.jpg`
 }
 
-// function changeDisplay(artwork){
-//     largeImage.src = artwork
-// }
 
 // function to display details on the left
 // function to add selected artwork to cart
